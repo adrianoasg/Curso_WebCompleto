f1d545499985890c6cac5d1be98b6bc19c855592
adrianoasg <adrianoasg@hotmail.com>
2019-04-09 14:58:33 -0300

    exercicios

--- fundamentos/destructuring2.js
+++ fundamentos/destructuring2.js
@@ -6,5 +6,5 @@ const [n1, , n3, , n5, n6 = 0] = [10, 7 , 9 ,8] //uma forma de pegar multiplos e
 //tb atribiu a várias variáveis usando apenas uma única atribuição
 console.log(n1, n3, n5, n6)
 
-const [, [, nota]] = [[, 8, 8], [9, 6, 8]] //extrai do segundavetor o segundo elemento
+const [, [, nota]] = [[, 8, 8], [9, 6, 8]] //extrai do segundo vetor o segundo elemento
 console.log(nota)
