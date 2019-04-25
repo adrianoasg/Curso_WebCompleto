9c7d7a1159859773d28fca62fc2ba898a1b1ebc7
adrianoasg <adrianoasg@hotmail.com>
2019-04-25 12:17:45 -0300

    projeto final

--- knowledge/backend/api/category.js
+++ knowledge/backend/api/category.js
@@ -3,15 +3,15 @@ module.exports = app => {
 
     const save = (req, res) => {
         const category = { ...req.body }
-        if(req.params.id) category.id = req.params.id
+        if (req.params.id) category.id = req.params.id
 
         try {
-            existsOrError(category.name, 'Nome não informado')
-        } catch(msg) {
+            existsOrError(category.name, 'Nome não informado.')
+        } catch (msg) {
             return res.status(400).send(msg)
         }
 
-        if(category.id) {
+        if (category.id) {
             app.db('categories')
                 .update(category)
                 .where({ id: category.id })
@@ -19,9 +19,9 @@ module.exports = app => {
                 .catch(err => res.status(500).send(err))
         } else {
             app.db('categories')
-            .insert(category)
-            .then(_ => res.status(204).send())
-            .catch(err => res.status(500).send(err))
+                .insert(category)
+                .then(_ => res.status(204).send())
+                .catch(err => res.status(500).send(err))
         }
     }
 
@@ -29,7 +29,7 @@ module.exports = app => {
         try {
             existsOrError(req.params.id, 'Código da Categoria não foi informado.')
 
-            const subcategory = await app.db('categories')
+            const subcategory = await app.db('categories.')
                 .where({ parentId: req.params.id })
             notExistsOrError(subcategory, 'Categoria possui subcategorias.')
 
@@ -38,11 +38,11 @@ module.exports = app => {
             notExistsOrError(articles, 'Categoria possui artigos.')
 
             const rowsDeleted = await app.db('categories')
-                .where({ id: req.params.id}).del()
+                .where({ id: req.params.id }).del()
             existsOrError(rowsDeleted, 'Categoria não foi encontrada.')
 
             res.status(204).send()
-        } catch(msg) {
+        } catch (msg) {
             res.status(400).send(msg)
         }
     }
@@ -57,7 +57,7 @@ module.exports = app => {
             let path = category.name
             let parent = getParent(categories, category.parentId)
 
-            while(parent) {
+            while (parent) {
                 path = `${parent.name} > ${path}`
                 parent = getParent(categories, parent.parentId)
             }
@@ -66,8 +66,8 @@ module.exports = app => {
         })
 
         categoriesWithPath.sort((a, b) => {
-            if(a.path < b.path) return -1
-            if(a.path > b.path) return 1
+            if (a.path < b.path) return -1
+            if (a.path > b.path) return 1
             return 0
         })
 
@@ -89,7 +89,7 @@ module.exports = app => {
     }
 
     const toTree = (categories, tree) => {
-        if(!tree) tree = categories.filter(c => !c.parentId)
+        if (!tree) tree = categories.filter(c => !c.parentId)
         tree = tree.map(parentNode => {
             const isChild = node => node.parentId == parentNode.id
             parentNode.children = toTree(categories, categories.filter(isChild))
